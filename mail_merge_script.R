2248ceff0369f7f2f2f2e5b9252bc635542370fd
Jerving Ryan <jerving@corp.bradleycenter.org>
2018-05-24 13:34:44 -0500

    Finished markdown and ran a merge on this list.

--- mail_merge_script.R
+++ mail_merge_script.R
@@ -1,15 +1,17 @@
 ## Packages
 library(knitr)
 library(rmarkdown)
+library(tinytex)
 
 ## Data
 personalized_info <- read.csv(file = "meeting-times.csv")
 
 ## Loop
-for (i in nrow(personalized_info)) {
-  rmarkdown::render(input = "MailMergeMarkdown.Rmd"),
-  output_format = "pdf_document",
-  output_file = paste("handout_",i, ".pdf", sep = ''),
+for (i in 1:nrow(personalized_info)){
+  rmarkdown::render(input = "MailMergeMarkdown.Rmd",
+  output_format = "html_document",
+  output_file = paste("handout_",i, ".html", sep = ''),
   output_dir = "handouts/")
 }
+
 
