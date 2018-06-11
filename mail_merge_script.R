e5714d1433f2ebf0261cf7fd4a4d4fa2e55bc425
Jerving Ryan <jerving@corp.bradleycenter.org>
2018-06-11 17:52:53 -0500

    Added LaTeX to desktop and so created pdf version of merge

--- mail_merge_script.R
+++ mail_merge_script.R
@@ -9,8 +9,8 @@ personalized_info <- read.csv(file = "meeting-times.csv")
 ## Loop
 for (i in 1:nrow(personalized_info)){
   rmarkdown::render(input = "MailMergeMarkdown.Rmd",
-  output_format = "html_document",
-  output_file = paste("handout_",i, ".html", sep = ''),
+  output_format = "pdf_document",
+  output_file = paste("handout_",i, ".pdf", sep = ''),
   output_dir = "handouts/")
 }
 
